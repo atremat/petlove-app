53ea6c83e6171bc0911870153748b9dc3b91c722
Artem <atremat@gmail.com>
2024-11-01 23:55:53 +0200

    notfoundpage added

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,20 @@
-import styles from './NotFoundPage.module.css';
+import { NavLink } from "react-router-dom";
+import styles from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
-  return <>Not found</>;
+  return (
+    <main className={styles.main}>
+      <div>
+        4
+        <div>
+          <img />
+        </div>
+        4
+      </div>
+      <p>Ooops! This page not found :(</p>
+      <NavLink to="/">To home page</NavLink>
+    </main>
+  );
 };
 
 export default NotFoundPage;
